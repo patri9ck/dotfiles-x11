81e1befa3b99495a4ee840fec92a9e49cbb8a3f1
patri9ck <patri9ck@gmail.com>
2024-04-04 02:13:23 +0200

    Specify hotkeys in fugitive.lua

--- .config/nvim/lua/keys.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>g", vim.cmd.Git)
--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,7 @@
 return {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+        vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+    end
 }
